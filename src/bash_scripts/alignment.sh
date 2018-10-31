337d38498827da38e30d472165e3e7fd1ea68238
Matthew Weber <mrweber2@illinois.edu>
2018-10-31 15:57:46 -0500

    Added new read group variables to task and workflow scripts

--- src/bash_scripts/alignment.sh
+++ src/bash_scripts/alignment.sh
@@ -33,6 +33,9 @@ read -r -d '' DOCS << DOCS
  USAGE:
  alignment.sh      -s           <sample_name> 
                    -p		<platform>
+                   -L           <library>
+                   -f           <flowcell_ID/platform_unit>
+                   -c           <sequencing_center>
                    -l           <read1.fq> 
                    -r           <read2.fq>
                    -G		<reference_genome>
@@ -46,10 +49,10 @@ read -r -d '' DOCS << DOCS
 
  EXAMPLES:
  alignment.sh -h
- alignment.sh -s sample -p platform -l read1.fq -r read2.fq -G reference.fa -K 10000000 -o "'-M'" -S /path/to/sentieon_directory -t 12 -P true -e /path/to/env_profile_file -d
+ alignment.sh -s sample -p platform -L library -f flowcell_ID -c center_name -l read1.fq -r read2.fq -G reference.fa -K 10000000 -o "'-M'" -S /path/to/sentieon_directory -t 12 -P true -e /path/to/env_profile_file -d
 
  NOTES: To prevent different results due to thread count, set -K to 10000000 as recommended by the Sentieon manual.
-        In order for getops to read in a string arguments for -o (extra_haplotyper_options), the argument needs to be quoted with a double quote (") followed by a single quote (').
+        In order for getops to read in a string arguments for -o (additional_bwa_options), the argument needs to be quoted with a double quote (") followed by a single quote (').
 
 #############################################################################
 
@@ -164,7 +167,7 @@ then
 fi
 
 ## Input and Output parameters
-while getopts ":hs:p:l:r:G:K:o:S:t:P:e:d" OPT
+while getopts ":hs:p:L:f:c:l:r:G:K:o:S:t:P:e:d" OPT
 do
         case ${OPT} in
                 h )  # Flag to display usage
@@ -179,6 +182,18 @@ do
                         PLATFORM=${OPTARG}
 			checkArg
                         ;;
+		L )  # Library name
+			LIBRARY=${OPTARG}
+			checkArg
+			;;
+		f )  # Platform unit / flowcell ID
+			PLATFORM_UNIT=${OPTARG}
+			checkArg
+			;;
+		c )  # Sequencing center name
+			CENTER_NAME=${OPTARG}
+			checkArg
+			;;
                 l )  # Full path to input read 1
                         INPUT1=${OPTARG}
 			checkArg
@@ -337,6 +352,21 @@ then
         EXITCODE=1
         logError "$0 stopped at line ${LINENO}. \nREASON=Missing sequencing platform option: -p"
 fi
+if [[ -z ${LIBRARY+x} ]]
+then
+	EXITCODE=1
+	logError "$0 stopped at line ${LINENO}. \nREASON=Missing sequencing library option: -L"
+fi
+if [[ -z ${PLATFORM_UNIT+x} ]]
+then
+	EXITCODE=1
+	logError "$0 stopped at line ${LINENO}. \nREASON=Missing platform unit / flowcell ID option: -f"
+fi
+if [[ -z ${CENTER_NAME+x} ]]
+then
+	EXITCODE=1
+	logError "$0 stopped at line ${LINENO}. \nREASON=Missing sequencing center name option: -c"
+fi
 if [[ -z ${BWA_OPTS+x}  ]]
 then
 	EXITCODE=1
@@ -402,7 +432,7 @@ if [[ "${IS_PAIRED_END}" == false ]] # Align single read to reference genome
 then
 	TRAP_LINE=$(($LINENO + 1))
 	trap 'logError " $0 stopped at line ${TRAP_LINE}. Sentieon BWA-MEM error in read alignment. " ' INT TERM EXIT
-	${SENTIEON}/bin/bwa mem ${BWA_OPTS_PARSED} -R "@RG\tID:$GROUP\tSM:${SAMPLE}\tPL:${PLATFORM}" -K ${CHUNK_SIZE} -t ${THR} ${REFGEN} ${INPUT1} > ${OUT} 2>>${TOOL_LOG}
+	${SENTIEON}/bin/bwa mem ${BWA_OPTS_PARSED} -R "@RG\tID:${GROUP}\tPU:${PLATFORM_UNIT}\tSM:${SAMPLE}\tPL:${PLATFORM}\tLB:${LIBRARY}\tCN:${CENTER_NAME}" -K ${CHUNK_SIZE} -t ${THR} ${REFGEN} ${INPUT1} > ${OUT} 2>>${TOOL_LOG}
 	EXITCODE=$?  # Capture exit code
 	trap - INT TERM EXIT
 
@@ -413,7 +443,7 @@ then
 else # Paired-end reads aligned
 	TRAP_LINE=$(($LINENO + 1))
 	trap 'logError " $0 stopped at line ${TRAP_LINE}. Sentieon BWA-MEM error in read alignment. " ' INT TERM EXIT
-	${SENTIEON}/bin/bwa mem ${BWA_OPTS_PARSED} -R "@RG\tID:$GROUP\tSM:${SAMPLE}\tPL:${PLATFORM}" -K ${CHUNK_SIZE} -t ${THR} ${REFGEN} ${INPUT1} ${INPUT2} > ${OUT} 2>>${TOOL_LOG} 
+	${SENTIEON}/bin/bwa mem ${BWA_OPTS_PARSED} -R "@RG\tID:$GROUP\tPU:${PLATFORM_UNIT}\tSM:${SAMPLE}\tPL:${PLATFORM}\tLB:${LIBRARY}\tCN:${CENTER_NAME}" -K ${CHUNK_SIZE} -t ${THR} ${REFGEN} ${INPUT1} ${INPUT2} > ${OUT} 2>>${TOOL_LOG} 
 	EXITCODE=$?  # Capture exit code
 	trap - INT TERM EXIT
 
--- src/wdl_scripts/Alignment/TestTasks/Runalignment.wdl
+++ src/wdl_scripts/Alignment/TestTasks/Runalignment.wdl
@@ -12,6 +12,9 @@ workflow RunAlignmentTask {
    String SampleName               # Name of the Sample
    String Group                    # starting read group string
    String Platform                 # sequencing platform for read group
+   String Library                  # sequencing library for read group
+   String CenterName               # sequencing center name for read group
+   Array[String] PlatformUnit      # One platform unit per alignment task
    Boolean PairedEnd               # Variable to check if single ended or not
 
    File Ref                        # Reference Genome
@@ -48,6 +51,9 @@ workflow RunAlignmentTask {
                SampleName=SampleName,
                Group=Group,
                Platform=Platform,
+               Library=Library,
+               CenterName=CenterName,
+               PlatformUnit=PlatformUnit,
                PairedEnd=PairedEnd,
                Sentieon=Sentieon,
                SentieonThreads=SentieonThreads,
@@ -72,6 +78,9 @@ workflow RunAlignmentTask {
                SampleName=SampleName,
                Group=Group,
                Platform=Platform,
+               Library=Library,
+               CenterName=CenterName,
+               PlatformUnit=PlatformUnit,
                PairedEnd=PairedEnd,
                Sentieon=Sentieon,
                SentieonThreads=SentieonThreads,
