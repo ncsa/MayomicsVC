d75f883fbc930ad22405fcde3322149780a13a5d
joshfactorial <joshfactorial@gmail.com>
2018-12-14 17:05:06 -0600

    updating bash script testing

--- testing/bash_script_testing/testing_call.bash
+++ testing/bash_script_testing/testing_call.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export TEST_PATH="$BATS_TEST_DIRNAME"
-export MAYOMICS_PATH="$(cd "$TEST_PATH/../.." && pwd)"
-export SCRIPT_PATH="$MAYOMICS_PATH/src/bash_scripts"
+export Mayomics_path="$(cd "$TEST_PATH/../.." && pwd)"
+export SCRIPT_PATH="$Mayomics_path/src/bash_scripts"
 export SCRIPT_TO_TEST=$1
+export Input_File_Path="$BATS_TEST_DIRNAME/../../.."
