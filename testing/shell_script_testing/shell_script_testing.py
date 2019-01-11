09dd9e92c2f63ea66b65a5ffdf7d928e79807731
joshfactorial <joshfactorial@gmail.com>
2019-01-11 17:39:06 -0600

    Updating code so it runs properly on iforge

--- testing/shell_script_testing/shell_script_testing.py
+++ testing/shell_script_testing/shell_script_testing.py
@@ -617,7 +617,7 @@ class TestArgs(ParameterizedTestCase):
         self.assertTrue(oct(os.stat('WGS_chr1_5X_E0.005_L1_read2.fastq.gz').st_mode)[-3:][1] == '4')
 
 
-    @unittest.skip("So slow")
+    # @unittest.skip("So slow")
     def test_successful_single_end_read(self):
         """
         This is simply a successful run of the tool and should be generalizable
